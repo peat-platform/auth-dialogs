3bffd0358aea8f8439ebab2305d014e94630fe10
sspatharioti <sspatharioti@velti.com>
2015-02-12 16:40:23 +0200

    getting types names from OPENi

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,7 @@ function postScript(method, postdata, path, addheaders, success, error) {
     }
 
     options = {
-        host: '10.130.34.17', //127.0.0.1
+        host: '127.0.0.1', // 10.130.34.17
         port: 443,
         path: path,
         method: method,
