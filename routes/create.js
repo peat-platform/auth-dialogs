be8de1463cfedbf307db13365aafb042936a0fb1
dmccarthy <dmccarthy@tssg.org>
2015-02-25 11:49:56 +0000

    Fixed register code

--- routes/create.js
+++ routes/create.js
@@ -1,6 +1,35 @@
 
+var jwt        = require('jwt-simple');
 var postScript = require('./postScript')
+var login      = require('./login');
 var seckeyenc  = 'oMF81IOFsZ0bvzSdcBVr';
+
+
+var checkPerms = function(req, res, next){
+
+   var redurl = req.session.redURL;
+   var path   = "/api/v1/permissions";
+
+   var headers = {
+      "Authorization": req.session.token
+   };
+
+   postScript("GET", {}, path, headers, function (resp_data) {
+
+      var newPerms = req.session.appPerms
+
+      if (deepEqual(resp_data, newPerms)){
+         var nexttt = redurl + "?OUST=" + req.session.token;
+         res.send(nexttt);
+      }
+      else{
+         res.send('https://' + req.headers.host + '/auth/permissions?OUST=OUST')
+      }
+
+   });
+
+}
+
 
 module.exports = function (req, res, next) {
    // post authorization to openi
@@ -13,7 +42,7 @@ module.exports = function (req, res, next) {
    var tok = jwt.decode(req.session.authtoken, seckeyenc);
 
    if (tok.hasOwnProperty("ip")) {
-      if (tok.ip == req.connection.remoteAddress) {
+      if (tok.ip == req.headers['x-forwarded-for'] || req.connection.remoteAddress) {
          validated = true;
       }
    }
@@ -22,7 +51,6 @@ module.exports = function (req, res, next) {
 
       //create user, then login to get session token
       var path1 = "/api/v1/auth/users";
-      var path2 = "/api/v1/auth/authorizations";
 
       var data = {
          "username": req.body.username,
@@ -34,29 +62,7 @@ module.exports = function (req, res, next) {
          if (typeof dat.error != 'undefined') {
             res.send(dat.error);
          } else {
-
-            var data2 = {
-               "username": req.body.username,
-               "password": req.body.password,
-               "api_key": req.session.api_key,
-               "secret": req.session.secret
-            };
-
-            postScript("POST", data2, path2, null, function (data3) {
-               //success
-               // redirect to redirectURI only if no error is found
-
-               if (typeof data3.error == 'undefined') {
-                  req.session.token = data3.session;
-
-                  res.send('OK');
-               } else {
-                  //OPENi returned error
-                  res.send(data3.error);
-               }
-            }, function () {
-               res.status(500).send('OPENi Internal error: session token generation failed');
-            });
+            login(req, res, next)
          }
       }, function () {
          //error;
